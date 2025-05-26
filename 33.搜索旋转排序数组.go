9d7fed7c156f4d601fa8becc2a203d6c6a726459
Wenhai Liu <wenhailiu@mail.ustc.edu.cn>
2025-05-26 16:09:31 +0800

    2025.5.26

--- 33.搜索旋转排序数组.go
+++ 33.搜索旋转排序数组.go
@@ -18,13 +18,13 @@ func search(nums []int, target int) int {
 			return mid
 		}
 		if nums[0] <= nums[mid] {
-			if nums[0] <= target && target < nums[mid] {
+			if nums[left] <= target && target < nums[mid] {
 				right = mid - 1
 			} else {
 				left = mid + 1
 			}
 		} else {
-			if nums[mid] < target && target <= nums[len(nums)-1] {
+			if nums[mid] < target && target <= nums[right] {
 				left = mid + 1
 			} else {
 				right = mid - 1
