73fe4c41a227e5c96456d820ee444ae328c746eb
Wenhai Liu <wenhailiu@mail.ustc.edu.cn>
2025-05-26 22:10:25 +0800

    2025.5.26

---
+++ 153.寻找旋转排序数组中的最小值.go
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode.cn id=153 lang=golang
+ *
+ * [153] 寻找旋转排序数组中的最小值
+ */
+
+// @lc code=start
+package lc
+
+func findMin(nums []int) int {
+	low, high := 0, len(nums)-1
+	for low < high {
+		pivot := low + (high-low)/2
+		if nums[pivot] < nums[high] {
+			high = pivot
+		} else {
+			low = pivot + 1
+		}
+	}
+	return nums[low]
+}
+
+// @lc code=end
